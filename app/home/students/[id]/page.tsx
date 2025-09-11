bb36524924aa792ac2ff5bb8f0d0052f086457af
AKHIL-GIREESH <akhilgireeshakg@gmail.com>
2025-09-11 15:34:50 +0530

    student id

--- app/home/students/[id]/page.tsx
+++ app/home/students/[id]/page.tsx
@@ -1,5 +1,7 @@
 "use client"
 
+import StatusDot from '@/app/(components)/statusDot';
+import { Button } from '@/components/ui/button';
 import { useParams } from 'next/navigation';
 
 export default function StudentIdPage() {
@@ -7,9 +9,41 @@ export default function StudentIdPage() {
     const { id } = params;
 
     return (
-        <div>
-            <h1>Student ID: {id}</h1>
+        <div className='w-[100%] h-[90vh] flex'>
+            <div className='w-[80%] h-[100%]'>
+                <div className='text-center mt-20'>
+                    <h1 className='text-4xl font-bold mb-2'>THE PERSON'S NAME</h1>
+                    <p>+91 9123456780</p>
+                </div>
+                <div className='flex justify-around mt-15 mb-15'>
+                    <div className='border w-[30%] rounded-xl h-[20vh] flex flex-col justify-center gap-5 items-center'>
+                        <p>Membership Type</p>
+                        <p className='text-2xl font-medium'>Basic</p>
+                    </div>
+                    <div className='border w-[30%] rounded-xl h-[20vh] flex flex-col justify-center gap-5 items-center'>
+                        <p>Start Date</p>
+                        <p className='text-2xl font-medium'>01-01-2000</p>
+                    </div>
+                    <div className='border w-[30%] rounded-xl h-[20vh] flex flex-col justify-center gap-5 items-center'>
+                        <p>End Date</p>
+                        <p className='text-2xl font-medium'>01-01-2000</p>
+                    </div>
+                </div>
+                <div className=' flex justify-center gap-10'>
+                    <Button className='w-[15%]'>
+                        Edit
+                    </Button>
 
+                    <Button className='w-[15%]'>
+                        Payment
+                    </Button>
+                </div>
+            </div>
+            <div className=' w-[20%]'>
+                <p className='text-end'>
+                    Active <StatusDot status={"Active"} />
+                </p>
+            </div>
         </div>
     );
 }
--- app/home/students/page.tsx
+++ app/home/students/page.tsx
@@ -5,11 +5,11 @@ const Students = () => {
         <div>
             Students
             <div className='flex max-w-[85vw] flex-wrap gap-10'>
-                <StudentCard name="S1" membershipType='Basic' status='Active' subscriptionEndDate={20} />
-                <StudentCard name="S2" membershipType='Premium' status='Active' subscriptionEndDate={20} />
-                <StudentCard name="S3" membershipType='Student' status='Active' subscriptionEndDate={10} />
-                <StudentCard name="S4" membershipType='Basic' status='Expired' subscriptionEndDate={5} />
-                <StudentCard name="S1" membershipType='Basic' status='Suspended' subscriptionEndDate={10} />
+                <StudentCard key={"1"} id={"1"} name="S1" membershipType='Basic' status='Active' subscriptionEndDate={20} />
+                <StudentCard key={"2"} id={"2"} name="S2" membershipType='Premium' status='Active' subscriptionEndDate={20} />
+                <StudentCard key={"3"} id={"3"} name="S3" membershipType='Student' status='Active' subscriptionEndDate={10} />
+                <StudentCard key={"4"} id={"4"} name="S4" membershipType='Basic' status='Expired' subscriptionEndDate={5} />
+                <StudentCard key={"5"} id={"5"} name="S1" membershipType='Basic' status='Suspended' subscriptionEndDate={10} />
             </div>
         </div>
     )
