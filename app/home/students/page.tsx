a34f5e5f6b3abcb6f9dc1d54bd7851c3c29aed47
AKHIL-GIREESH <akhilgireeshakg@gmail.com>
2025-09-11 15:48:57 +0530

    search ui

--- app/home/students/page.tsx
+++ app/home/students/page.tsx
@@ -1,9 +1,19 @@
+"use client"
+
 import StudentCard from "@/app/(components)/studentCard"
+import { Input } from "@/components/ui/input"
+import { Search } from "lucide-react"
+import { useState } from "react"
 
 const Students = () => {
+
+    const [search, setSearch] = useState("")
     return (
         <div>
             Students
+
+            <Input className="rounded-full w-[60%]" type="text" value={search} placeholder="Search" onChange={(e) => setSearch(e.target.value)} />
+            <br />
             <div className='flex max-w-[85vw] flex-wrap gap-10'>
                 <StudentCard key={"1"} id={"1"} name="S1" membershipType='Basic' status='Active' subscriptionEndDate={20} />
                 <StudentCard key={"2"} id={"2"} name="S2" membershipType='Premium' status='Active' subscriptionEndDate={20} />
